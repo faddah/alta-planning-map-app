d72224f83898861f252cf4664a1eb849526598af
Faddah Wolf <my_biz@me.com>
2018-01-21 08:41:16 -0800

    got it to the point where map is working and UI for app is looking ok, need to still hook up buttons & text area & make drop-down work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Alta Planning Map App';
+  zoom = 12;
+
+  // initial center map position
+  lat = 45.515160;
+  lng = -122.594967;
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
+  }
+
+  mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    });
+  }
+
+  markerDragEnd(m: marker, $event: MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  markers: marker[] = [
+	  {
+		  lat: 45.515160,
+		  lng: -122.594967,
+		  label: 'A',
+		  draggable: true
+	  },
+	  {
+		  lat: 45.517921,
+		  lng: -122.660986,
+		  label: 'B',
+		  draggable: true
+	  },
+	  {
+		  lat: 45.517113,
+		  lng: -122.675333,
+		  label: 'C',
+		  draggable: true
+	  }
+  ];
+}
+
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAU0L7yJfoe8ix7LCm9Q-9UAfbPSUKodLM'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
